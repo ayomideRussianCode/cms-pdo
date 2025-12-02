00f6dab5a7acb557750a0c2f44709fb2e5878256
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-12-02 04:46:03 +0100

    autoloading

--- classes/Database.php
+++ classes/Database.php
@@ -17,13 +17,13 @@ class Database {
 
         try{
 
-           $this->conn = new PDO("mysql:host = {$this->host};dbname={$this->name}, $this->username, $this->password");
+           $this->conn = new PDO("mysql:host = {$this->host};dbname={$this->db_name}", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         }catch (PDOException $exception) {
 
             echo "Connection error". $exception->getMessage();
-            
+
         }
 
         return $this->conn;
--- init.php
+++ init.php
@@ -1,12 +1,16 @@
 <?php
+
+//autoload classes
+require_once "autoloader.php";
+
 //start session
 session_start();
 
 //includes the main config file
 require_once "config/config.php";
 
-//load database
-require_once "classes/Database.php";
+// //load database
+// require_once "classes/Database.php";
 
 //include helper functions
 require_once "helpers.php";
